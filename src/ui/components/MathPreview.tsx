88f2181066f01b7759bd00f3c2a7e3709b6efca5
Ramón Otero <kaptux@gmail.com>
2021-04-14 01:07:19 +0200

    Math preview positioning

--- src/ui/components/MathPreview.tsx
+++ src/ui/components/MathPreview.tsx
@@ -11,6 +11,42 @@ function MathPreview() {
   const spanRef = useRef();
   const visible = position != NO_POSITION;
 
+  const searchStyle = function (cm, line, style) {
+    const handle = cm.getLineHandle(line);
+    let lastChar = 0;
+    for (const styleIn of handle.styles) {
+      if (typeof styleIn === "number") {
+        lastChar = styleIn;
+      } else if (styleIn && styleIn.indexOf(style) >= 0) {
+        return lastChar;
+      }
+    }
+    return 0;
+  };
+
+  const getCursorPosition = function (cm, exp) {
+    const cursor = cm.getCursor();
+    let { line, ch } = cursor;
+    let lineContent = cm.getLine(line);
+    let isBlock = lineContent == exp || lineContent.startsWith("$$");
+    if (isBlock) {
+      ch = 0;
+      let index = searchStyle(cm, line, "formatting-math-end");
+      while (index == 0) {
+        line += 1;
+        index = searchStyle(cm, line, "formatting-math-end");
+      }
+    } else {
+      let index = lineContent.indexOf(exp) + exp.length + 1;
+      while (ch > index) {
+        index = lineContent.indexOf(exp, index) + exp.length + 1;
+      }
+      ch = index - exp.length - 1;
+    }
+
+    return cm.charCoords({ line, ch }, "window");
+  };
+
   useEffect(() => {
     if (mathPreview) {
       const { cm, exp } = mathPreview;
@@ -25,7 +61,7 @@ function MathPreview() {
         if (!mathRenderer.current.isReady()) {
           return;
         }
-        const { left, top } = cm.charCoords(cm.getCursor(), "window");
+        const { left, top } = getCursorPosition(cm, exp);
         mathRenderer.current.startRender(exp);
         if (!visible) {
           setPosition({ left, top: top + LINE_HEIGHT });
